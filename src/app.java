60c36ddf1716c32793dbb3c3c3192e39812c2d48
mayankmetha <mayankmethad@gmail.com>
2018-10-27 17:41:26 +0530

    fix gitignore test base code

--- src/app.java
+++ src/app.java
@@ -1,4 +1,10 @@
-import org.apache.zookeeper.*;
+import org.apache.zookeeper.ZooKeeper;
 
 public class app {
+
+    private ZooKeeper zk;
+
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+    }
 }
